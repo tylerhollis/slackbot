79e10f2745f8809386e26c94954a75c094442a08
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-25 23:36:34 -0400

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+module.exports = function(robot) {
+  robot.hear(/Will you help me with my music career?/i, function(res) {});
+  return robot.respond(/First you have to purify yourself in the waters of Lake Minnetonka./i, function(res) {});
+};
+
