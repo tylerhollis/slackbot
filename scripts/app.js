458fb7b58fbb4f4058c43795625756abc752190a
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-25 23:13:48 -0400

    Initial Commit

--- scripts/app.js
+++
@@ -1,3 +0,0 @@
-kevintherobot.respond(/Will you help me with my music career?/, function(res)
-	{return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");
-	});
--- scripts/example.coffee
+++ scripts/example.coffee
@@ -10,6 +10,7 @@
 
 module.exports = (robot) ->
 
+
   # robot.hear /badger/i, (res) ->
   #   res.send "Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS"
   #
