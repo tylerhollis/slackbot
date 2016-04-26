b9da9434164f53a674db1fa988244855b490e454
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-26 16:41:31 -0400

    Initial Commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,18 @@
-// module.exports = function(robot) {
-// 	robot.respond(/Will you help me with my music career?/, function(res) {
-//     return res.send(/"First you have to purify yourself in the waters of Lake Minnetonka."/);  
-// 	});
-// };
+// Ask bot "Will you help me with my music career?" and get a response from Prince.
+// Ask bot who is cooler and get randomized response.
 
 module.exports = function(robot) {
-  return robot.hear(/Call/i, function(res) {
-    return res.send("Response");
+  return robot.hear(/Will you help me with my music career?/i, function(res) {
+    return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");
+  });
+  return robot.hear(/Who is (better|worse)\?* (.*) or (.*?)\??$/i, function(msg) {
+    var botAnswers;
+    botAnswers = msg.random([1, 2, 3, 4, 5]);
+    if (botAnswers >= 3) {
+      return msg.send(msg.random(uhh_what));
+    } else {
+      return msg.send("Clearly " + msg.match[botAnswers + 2] + " is " + msg.match[2]);
+    }
   });
 };
+
