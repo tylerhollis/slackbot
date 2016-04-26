1f9d188e9a8d293562820dc6c0ae2b5f639000ec
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-26 18:26:30 -0400

    Initial Commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,16 @@
 // Ask bot "Will you help me with my music career?" and get a response from Prince.
-// Ask bot who is cooler and get randomized response.
 
 module.exports = function(robot) {
   return robot.hear(/Will you help me with my music career?/i, function(res) {
     return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");
   });
-var choosen_response;
-
-choosen_response = msg.random([1, 2, 3, 4, 5]);
-
-if (choosen_response >= 3) {
-  msg.send(msg.random(uhh_what));
-} else {
-  msg.send("Clearly " + msg.match[choosen_response + 2] + " is " + msg.match[2]);
-}
+  return bot.hear(/Hi Hubot! My name is (.*)/i, function(msg) {
+  		var name;
+  		name = msg.match[0];
+  		if (name == "Hubot") ){
+  			return msg.send("You're not Hubot--I'm Hubot!");    
+  		} else {
+			return msg.reply("Nice to meet you, " + name + "!");
+		}
+	});
 };
-
-// return msg.send("Clearly " + msg.match[botAnswers + 2] + " is " + msg.match[2]);
