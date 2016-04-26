d246ef6cdd7cb1963a2c80e3d1d305ab357a7e9b
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-26 00:22:54 -0400

    O(+>

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 module.exports = function(robot) {
-  robot.hear(/Will you help me with my music career?/i, function(res) {});
-  return robot.respond(/First you have to purify yourself in the waters of Lake Minnetonka./i, function(res) {});
+	bot.respond(/Will you help me with my music career?/, function(res) {
+    return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");  
+	});
 };
 
