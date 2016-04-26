c758af46a8fd942942bbb6384a9a59b20a3ba4a9
Tyler Derryberry <tylerhollis@gmail.com>
2016-04-26 17:07:50 -0400

    Initial Commit

--- scripts/app.js
+++ scripts/app.js
@@ -5,14 +5,15 @@ module.exports = function(robot) {
   return robot.hear(/Will you help me with my music career?/i, function(res) {
     return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");
   });
+var choosen_response;
 
-  return robot.hear(/Who is (better|worse)\?* (.*) or (.*?)\??$/i, function(msg) {
-    var botAnswers;
-    botAnswers = msg.random([1, 2, 3, 4, 5]);
-    if (botAnswers >= 3) {
-      return msg.send(msg.random(uhh_what));
-    } else {
-      return msg.send("Clearly " + msg.match[botAnswers + 2] + " is " + msg.match[2]);
-    }
-  });
+choosen_response = msg.random([1, 2, 3, 4, 5]);
+
+if (choosen_response >= 3) {
+  msg.send(msg.random(uhh_what));
+} else {
+  msg.send("Clearly " + msg.match[choosen_response + 2] + " is " + msg.match[2]);
+}
 };
+
+// return msg.send("Clearly " + msg.match[botAnswers + 2] + " is " + msg.match[2]);
