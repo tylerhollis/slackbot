3e29a38b7d6da010ed5936dd68b0556b320043e9
Tyler Derryberry <tylerhollis@gmail.com>
2016-05-01 20:09:53 -0400

    fix multiple returns

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 // Ask bot "Will you help me with my music career?" and get a response from Prince.
 
 module.exports = function(robot) {
-  return robot.hear(/Will you help me with my music career?/i, function(res) {
+  robot.hear(/Will you help me with my music career?/i, function(res) {
     return res.send("First you have to purify yourself in the waters of Lake Minnetonka.");
   });
-  return robot.hear(/Hi Hubot! My name is (.*)/i, function(msg) {
+  robot.hear(/Hi Hubot! My name is (.*)/i, function(msg) {
   		var name;
   		name = msg.match[0];
   		if (name == "Hubot") {
